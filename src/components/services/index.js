4c63f60e079428c0da7dbd4bcea20538a2b6a2a2
Lolaknowsbest <Victoryonuoha@cmail.carleton.ca>
2022-02-05 23:43:39 -0700

    modified services section for mobile responsivness

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -16,19 +16,39 @@ function Services() {
             <section className={styles.projectcard}>
                 <div className={styles.filterDiv}>
                     <img src={cataract} />
-                    <p>CATARACT SURGERY</p>
+                    <article>
+                    <h4>CATARACT SURGERY</h4>
+                    <p>Cataract surgery is a procedure to remove the
+                         lens of your eye and, in most cases, replace it with an artificial lens. 
+                       <br /> <br/> Normally, the lens of your eye is clear. A cataract causes the lens to become cloudy,
+                         which eventually affects your vision</p>
+                    </article>
                 </div>
                 <div className={styles.filterDiv }>
                     <img src={cornea} />
-                    <p>CORNEA SURGERY</p>
+                    <article>
+                    <h4>CORNEA SURGERY</h4>
+                    <p>A cornea transplant can restore vision, reduce pain, 
+                        and improve the appearance of a damaged or diseased cornea.
+                        Your cornea is the transparent, dome-shaped surface of your eye. 
+                        It's where light enters your eye and affects eye's ability to see clearly.</p>
+                    </article>
                     </div>
                 <div className={styles.filterDiv}>
                     <img src={comprehensive} />
-                    <p>COMPREHENSIVE OPTHAMALOGY</p>
+                    <article>
+                    <h4>COMPREHENSIVE OPTHAMALOGY</h4>
+                    <p>This service handles general eye exams and common eye conditions  
+                    We provide screening examinations for patients with chronic illnesses. Patients diagnosed with serious eye conditions are referred to the appropriate subspecialty area</p>
+                    </article>
                     </div>
                 <div className={styles.filterDiv}>
                     <img src={consult} />
-                    <p>VISION CONSULT</p>
+                    <article>
+                    <h4>VISION CONSULT</h4>
+                    <p>The objective of pre-operative consultations is to confirm candidacy, which is determined using safe, painless diagnostic technology and testing. 
+                   <br/><br/> During your procedure, our team will also discuss the cost of your procedure as well as our financing options.</p>
+                    </article>
                     </div>
                
             </section>
