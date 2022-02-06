0067cfbc3339385bff8e6b92d22477a775ae1a16
Lolaknowsbest <Victoryonuoha@cmail.carleton.ca>
2022-02-06 01:27:42 -0700

    modifiedcolor pallette

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -5,14 +5,31 @@ import ContactStyle from './contact.module.scss';
 function Contact() {
     return (
         <div className={ContactStyle.contactContainer} id="contactHash">
-            <div>
-                <h1>CONTACT US</h1>
-            </div>
-            <div className={ContactStyle.contactInfo}>
+            
+                
+            
+            <section className={ContactStyle.contactInfo}>
+            <h1>CONTACT US</h1>
                 <p><strong>Location: </strong>321 Best Street</p>
                 <p><strong>Phone Number: </strong>123-456-7890</p>
                 <p><strong>Fax Number: </strong>098-765-4321</p>
-            </div>
+                <p><strong>Email: </strong>tribecaeyecare@gmail.com</p>
+            </section>
+            <section>
+           <h1>HOURS</h1> 
+      <p>Monday: 8:30am - 5:00pm</p>  
+
+      <p>Tuesday: 8:30am - 5:00pm</p>  
+
+        <p>Wednesday & Thursday: 8:30am - 7:00pm</p>
+
+       <p>Friday: 8:30am - 5:00pm</p> 
+
+       <p>Saturday: 9:00am - 3:00pm</p> 
+
+      <p> Sunday: CLOSED</p> 
+            </section>
+        
         </div>
     )
 }
